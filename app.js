3edb1cbb12a77db2aa367d5dcffd5b32c80e83ef
Mike Blackburn <jhb.anaesthetist@gmail.com>
2021-01-02 12:01:27 +0200

    first app.js commit

---
+++ app.js
@@ -0,0 +1,4 @@
+//set the outputs for the random message generator - requires 3
+const firstOutput = ['aries', 'taurus', 'gemini', 'cancer', 'leo', 'virgo', 'libra', 'scorpio', 'saggitarius', 'capricorn', 'aquarius', 'pisces'];
+const secondOutput = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const thirdOutput = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
